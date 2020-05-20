b5f523db1bbdf307f24e81fe7705d4ce62fb13f9
Dustin Myers <dustinblaine.myers@gmail.com>
2020-05-20 16:41:39 -0400

    game now working

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -8,12 +8,13 @@ class MainGame extends Component {
       button2toggle: "buttonInner2",
       button3toggle: "buttonInner3",
       button4toggle: "buttonInner4",
-      patternArray: [1, 2, 3, 4],
+      patternArray: [1, 1, 1, 1],
       gameMessage: "test",
       index: 0,
       ready: false,
       lose: false,
       win: false,
+      winCount: 1,
     };
   }
 
@@ -33,6 +34,21 @@ class MainGame extends Component {
         if (this.state.patternArray.length === 0) {
           this.setState({ win: true });
           this.setState({ ready: false });
+          this.setState({ winCount: this.state.winCount + 1 });
+          const newPattern = () => {
+            let workingArray = [];
+            this.setState({ index: 0 });
+            for (let winCount = this.state.winCount; winCount > 0; winCount--) {
+              workingArray.push(Math.floor(Math.random() * 3 + 1));
+            }
+            console.log(workingArray);
+            return workingArray;
+          };
+          const anotherPattern = newPattern();
+
+          this.setState({ patternArray: anotherPattern });
+          this.timerID = setInterval(() => this.showPattern(), 1000);
+          this.setState({ ready: true });
         }
       }
 
@@ -69,7 +85,7 @@ class MainGame extends Component {
     this.setState({ [toggle]: button + " Active" });
     setTimeout(() => {
       this.setState({ [toggle]: button });
-    }, 1000);
+    }, 500);
   }
 
   componentDidMount() {
